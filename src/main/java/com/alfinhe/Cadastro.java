ae55f08b136270cb88d3053c4f77ecdfee1943f1
Ualfinhe Barbosa <147889889+hgfxxx53@users.noreply.github.com>
2024-12-01 07:29:15 +0000

    Update Cadastro.java

--- src/main/java/com/alfinhe/Cadastro.java
+++ src/main/java/com/alfinhe/Cadastro.java
@@ -1,4 +1,34 @@
 package com.alfinhe;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cadastro {
-}
+        private List <Usuario> usuarios;
+
+        public Cadastro() {
+            this.usuarios = new ArrayList <> ();
+        }
+
+        public void adicionarUsuario(Usuario usuario) {
+            usuarios.add(usuario);
+            System.out.println("Usuário cadastrado com sucesso!");
+        }
+
+        public void listarUsuarios() {
+            for (Usuario usuario : usuarios) {
+                System.out.println("Nome: " + usuario.getNome());
+                System.out.println("Email: " + usuario.getEmail());
+            }
+        }
+
+        public Usuario buscarUsuarioPorEmail(String email) {
+            for (Usuario usuario : usuarios) {
+                if (usuario.getEmail().equals(email)) {
+                    return usuario;
+                }
+            }
+            return null;
+        }
+    }
